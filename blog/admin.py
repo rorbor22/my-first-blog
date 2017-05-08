65e2e0d170c7a3da2d8062b6818d0469a338b4ea
Your Name <bor@debian7-6>
2017-05-08 07:19:19 -0400

    My Django Girls app, first commit

---
+++ blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import blog
+
+admin.site.register(blog)
+
+# -*- coding: utf-8 -*-
+##from __future__ import unicode_literals
+
+## from django.contrib import admin
+
+# Register your models here.
